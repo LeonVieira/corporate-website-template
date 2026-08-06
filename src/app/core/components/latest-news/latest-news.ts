af2d0002d34f5adf324146177f7dfc9a2f78692f
Leon Vieira <leonvieira456@gmail.com>
2026-08-06 19:14:32 +0200

    Add models and services for company website sections

---
+++ src/app/core/components/latest-news/latest-news.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-news',
+  imports: [],
+  templateUrl: './latest-news.html',
+  styleUrl: './latest-news.css',
+})
+export class LatestNews {
+
+}
---
+++ src/app/core/models/why-choose-us.model.ts
@@ -0,0 +1,5 @@
+export interface WhyChooseUs {
+    title: string;
+    description: string;
+    icon: string;
+}
